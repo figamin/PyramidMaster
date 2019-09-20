689af0a10dd9c7e0624d84775fcd5a8cbfc40676
figman57 <ian57anderson@gmail.com>
2019-09-20 08:36:23 -0400

    Hey
    Signed-off-by: figman57 <ian57anderson@gmail.com>

--- pmaster.py
+++ pmaster.py
@@ -1,57 +1,6 @@
 import serial
-# opcode constants
-# MODES
-SAFE_MODE = "131"
-FULL_MODE = "132"
-# CLEANING
-CLEAN = "135"
-MAX = "136"
-SPOT = "134"
-SEEK_DOCK = "143"
-POWER_OFF = "133"
-# ACTUATOR
-DRIVE = "137"
-DRIVE_DIRECT = "145"
-MOTORS = "138"
-LED = "139"
-SONG = "140"
-# SPECIAL DRIVE VALUES
-DRIVE_STRAIGHT_VALUE = 32768
-DRIVE_CLOCKWISE_VALUE = -1
-DRIVE_COUNTERCLOCKWISE_VALUE = 1
-# SPECIAL MOTOR VALUES
-SIDEBRUSH_COUNTERCLOCKWISE_VALUE = 0
-SIDEBRUSH_CLOCKWISE_VALUE = 3
-MAINBRUSH_INWARD_VALUE = 2
-MAINBRUSH_OUTWARD_VALUE = 4
-VACUUM_VALUE = 1
-# INPUT COMMANDS
-SENSORS = "142"
-QUERY_LIST = "149"
-STREAM = "148"
-PAUSE_RESUME_STREAM = "150"
-# PACKET ID LIST
-BUMPS_AND_WHEEL_DROPS = "7"
-WALL = "8"
-CLIFF_LEFT = "9"
-CLIFF_FRONT_LEFT = "10"
-CLIFF_FRONT_RIGHT = "11"
-CLIFF_RIGHT = "12"
-VIRTUAL_WALL = "13"
-WHEEL_OVERCURRENTS = "14"
-DIRT_DETECT = "15"
-INFARED_CHAR_OMNI = "17"
-INFARED_CHAR_LEFT = "52"
-INFARED_CHAR_RIGHT = "53"
-DISTANCE_TRAVELLED = "19"
-DEGREES_TURNED = "20"
-BATTERY_VOLTAGE_MV = "22"
-BATTERY_CURRENT_MA = "23"
-BATTERY_TEMPERATURE_CELSIUS = "24"
-BATTERY_CHARGE_MAH = "25"
-BATTERY_CAPACITY_MAH = "26"
+import ropcodes
 connect = None
-
 
 """Converts given integer into a tuple of the corresponding two's compliment."""
 def int_to_twos_complement(num):
@@ -73,10 +22,10 @@ def int_to_twos_complement(num):
 """Drives the Roomba using given velocity and turning radius."""
 def drive_command(velocity, radius):
     if (velocity <= 500 or velocity >= -500)\
-            and (radius <= 2000 or radius >= 2000 or radius == DRIVE_STRAIGHT_VALUE):
+            and (radius <= 2000 or radius >= 2000 or radius == ropcodes.DRIVE_STRAIGHT_VALUE):
         velocity_1, velocity_2 = int_to_twos_complement(velocity)
         radius_1, radius_2 = int_to_twos_complement(radius)
-        send_commands(DRIVE + " " + velocity_1 + " " + velocity_2 + " " + radius_1 + " " + radius_2)
+        send_commands(ropcodes.DRIVE + " " + velocity_1 + " " + velocity_2 + " " + radius_1 + " " + radius_2)
 
 
 """Drives the roomba using given right and left wheel velocities"""
@@ -84,8 +33,18 @@ def drive_direct_command(right_velocity, left_velocity):
     if 500 >= (right_velocity and left_velocity) >= -500:
         right_1, right_2 = int_to_twos_complement(right_velocity)
         left_1, left_2 = int_to_twos_complement(left_velocity)
-        send_commands(DRIVE_DIRECT + " " + right_1 + " " + right_2 + " " + left_1 + " " + left_2)
+        send_commands(ropcodes.DRIVE_DIRECT + " " + right_1 + " " + right_2 + " " + left_1 + " " + left_2)
 
+
+def wall_or_cliff_seen():
+    send_commands(ropcodes.QUERY_LIST + " 5 " + ropcodes.WALL + " "
+    + ropcodes.CLIFF_LEFT + " " + ropcodes.CLIFF_FRONT_LEFT + " "
+    + ropcodes.CLIFF_FRONT_RIGHT + " " + ropcodes.CLIFF_RIGHT)
+    wall_status = connect.read(1)
+    if wall_status is 0:
+        return False
+    else:
+        return True
 
 # def motors_command(selected_motor, power_state)
 """Sends given commands to the Roomba."""
